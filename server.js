415dd98b2b9f3451d6f4639d6ae72aa63c0a938d
Victor <victorgestaltung@gmail.com>
2024-03-11 12:11:44 -0600

    items

--- server.js
+++ server.js
@@ -23,9 +23,38 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/generate-rates', async (req, res) => {
-  console.log(req.body)
+  console.log('tems', req.body.items)
   try{
-    let rates = { "rates": [ { "service_name": "canadapost-overnight", "service_code": "ON", "total_price": "1295", "description": "This is the fastest option by far", "currency": "CAD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2013-04-12 14:48:45 -0400" }, { "service_name": "fedex-2dayground", "service_code": "2D", "total_price": "2934", "currency": "USD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2013-04-12 14:48:45 -0400" }, { "service_name": "fedex-priorityovernight", "service_code": "1D", "total_price": "3587", "currency": "USD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2013-04-12 14:48:45 -0400" } ] }
+    let rates = { 
+      "rates": [ 
+        { 
+          "service_name": "canadapost-overnight", 
+          "service_code": "ON", 
+          "total_price": "1295", 
+          "description": "This is the fastest option by far", 
+          "currency": "CAD",
+          "min_delivery_date": "2013-04-12 14:48:45 -0400",
+          "max_delivery_date": "2013-04-12 14:48:45 -0400" 
+        },
+        { 
+          "service_name": "fedex-2dayground", 
+          "service_code": "2D", 
+          "total_price": "2934", 
+          "currency": "USD", 
+          "min_delivery_date": 
+          "2013-04-12 14:48:45 -0400", 
+          "max_delivery_date": "2013-04-12 14:48:45 -0400" 
+        }, 
+        { 
+          "service_name": "fedex-priorityovernight", 
+          "service_code": "1D", 
+          "total_price": "3587", 
+          "currency": "USD", 
+          "min_delivery_date": "2013-04-12 14:48:45 -0400", 
+          "max_delivery_date": "2013-04-12 14:48:45 -0400" 
+        } 
+      ] 
+    }
     
     res.send(rates)
     return rates
